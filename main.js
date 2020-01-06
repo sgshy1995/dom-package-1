74b08d23dff339d16208c5250769a7eca0d5f9ee
Singlesaul <singlesaulwork@gmail.com>
2020-01-06 18:29:23 +0800

    update

---
+++ main.js
@@ -0,0 +1,23 @@
+
+window.dom = {
+    find(nodes) {
+        return document.querySelectorAll(nodes)
+    },
+    style(node, property, type) {
+        node.style[`${property}`] = type
+    },
+    each(nodes, fn) {
+        let array = []
+        for (let i = 0; i < nodes.length; i++){
+            array.push(nodes[i])
+        }
+        array.forEach(fn)
+    }
+}
+
+const ele = dom.find('#block')[0] // 获取对应的元素
+console.log(ele)
+dom.style(ele,'color','red') // 设置 div.style.color
+
+const divList = dom.find('.red') // 获取多个 div.red 元素
+dom.each(divList, (n)=> console.log(n)) // 遍历 divList 里的所有元素
